8c64ae0383450e3c2bdcf96b5007c2f047da751e
wtklbm <wtklbm@gmail.com>
2020-05-30 23:42:32 +0800

    fix display error

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,86 @@
-import * as vscode from 'vscode';
+import {
+    Range,
+    ExtensionContext,
+    TextEditor,
+    TextEditorEdit,
+    commands,
+    Position,
+    workspace,
+} from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.swimming.closeWriteCode', function (
-        textEditor:vscode.TextEditor,edit:vscode.TextEditorEdit,args
-    ){
-        vscode.commands.executeCommand('workbench.action.reloadWindow');
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.swimming.rewriteCode', function (
-        textEditor:vscode.TextEditor,edit:vscode.TextEditorEdit,args
-    ) {
-        let selectionRange = new vscode.Range(textEditor.selection.start,textEditor.selection.end);
-        if(selectionRange.isEmpty){
-            const end = new vscode.Position(textEditor.document.lineCount + 1, 0);
-            selectionRange = new vscode.Range(new vscode.Position(0, 0), end);
+function createPosition(textEditor: TextEditor) {
+    const { line, character } = textEditor.selection.start;
+
+    return new Position(line, character);
+}
+
+function getReWriteSpeed() {
+    const reWriteSpeed = workspace
+        .getConfiguration()
+        .get<number>('vscodePluginSwimming.reWriteSpeed');
+
+    return typeof reWriteSpeed === 'number' ? reWriteSpeed : 0;
+}
+
+function rewriteCode(
+    textEditor: TextEditor,
+    edit: TextEditorEdit,
+    _args: any[]
+) {
+    const { start, end } = textEditor.selection;
+
+    let i = 0;
+    let selectionRange = new Range(start, end);
+
+    if (selectionRange.isEmpty) {
+        const end = new Position(textEditor.document.lineCount + 1, 0);
+        selectionRange = new Range(new Position(0, 0), end);
+    }
+
+    const beforeText = textEditor.document.getText(selectionRange);
+
+    edit.delete(selectionRange);
+
+    const inputInterval: NodeJS.Timeout = setInterval(() => {
+        if (i >= beforeText.length || textEditor.document.isClosed) {
+            return clearInterval(inputInterval);
         }
-        const beforeText = textEditor.document.getText(selectionRange);
-        let i=0;
-        edit.delete(selectionRange);
-        let writePosition = new vscode.Position(
-            textEditor.selection.start.line,
-            textEditor.selection.start.character
-        );
-        const speed = vscode.workspace.getConfiguration().get<number>('vscodePluginSwimming.reWriteSpeed')
-        const inputInterval = setInterval(()=>{
-            if(i>=beforeText.length || textEditor.document.isClosed){
-                return clearInterval(inputInterval)
-            }
-            if(beforeText[i]==='\r' && beforeText[i+1]==='\n') {i++;return;}
-            textEditor.edit(editBuilder => {
-                editBuilder.insert(writePosition, beforeText[i]);
-                if(beforeText[i]==='\n') {
-                    writePosition = new vscode.Position(writePosition.line+1,0)
-                } else {
-                    writePosition = new vscode.Position(writePosition.line,writePosition.character+1)
-                }
-                i++;
-			});
-        },speed?speed:0);
-    }));
+
+        if (beforeText.startsWith('\r\n', i)) {
+            return i++;
+        }
+
+        textEditor.edit((editBuilder) => {
+            editBuilder.insert(createPosition(textEditor), beforeText[i++]);
+        });
+    }, getReWriteSpeed());
+}
+
+function closeWriteCode(
+    _textEditor: TextEditor,
+    _edit: TextEditorEdit,
+    ..._args: any[]
+) {
+    commands.executeCommand('workbench.action.reloadWindow');
+}
+
+export function activate(context: ExtensionContext) {
+    const textEditorCommandMap = [
+        {
+            command: 'extension.swimming.rewriteCode',
+            callback: rewriteCode,
+        },
+        {
+            command: 'extension.swimming.closeWriteCode',
+            callback: closeWriteCode,
+        },
+    ];
+
+    context.subscriptions.push(
+        ...textEditorCommandMap.map(({ command, callback }) => {
+            return commands.registerTextEditorCommand(command, callback);
+        })
+    );
 }
 
 export function deactivate() {}
