95bf677271d64ba0584e916a4de5332ab4ea7036
tomzhangyuan <tomzhangyuan@didiglobal.com>
2023-04-18 11:55:16 +0800

    feat: 升级模式切换功能

--- src/extension.ts
+++ src/extension.ts
@@ -17,6 +17,27 @@ function getReWriteSpeed() {
 
     return typeof reWriteSpeed === 'number' ? reWriteSpeed : 0;
 }
+
+
+enum RewriteMode {
+    Cycle = 'cycle', // 循环写
+    Once = 'once' // 写一次结束
+}
+
+function getRewriteMode():RewriteMode {
+    const nowRewriteMode = workspace
+        .getConfiguration()
+        .get<RewriteMode>('vscodePluginSwimming.rewriteMode');
+        console.log(666666, nowRewriteMode)
+    return nowRewriteMode || RewriteMode.Once;
+}
+
+function setRewriteMode(nowRewriteMode:RewriteMode) {
+    return workspace
+        .getConfiguration()
+        .update('vscodePluginSwimming.rewriteMode', nowRewriteMode)
+}
+
 const isWriteCodePauseMap:Map<string,boolean> = new Map();
 const isWritingCodeMap:Map<string,boolean> = new Map();
 const showPauseinfo = function(textEditor: TextEditor) {
@@ -24,33 +45,53 @@ const showPauseinfo = function(textEditor: TextEditor) {
         window.showInformationMessage('pauseWriteCode Now')
     }
 }
-function rewriteCode(
+
+function getSelectionRangeByStartAndEnd({
+    start, 
+    end, 
+    textEditor
+}:{
+    start:Position, 
+    end:Position, 
     textEditor: TextEditor,
-    edit: TextEditorEdit,
-    _args: any[]
-) {
-    if(isWritingCodeMap.get(textEditor.document.fileName)) {
-        return window.showInformationMessage('rewriteCode already in progress')
-    }
-    isWritingCodeMap.set(textEditor.document.fileName,true)
-    isWriteCodePauseMap.set(textEditor.document.fileName,false)
-    const { start, end } = textEditor.selection;
-    let i = 0;
+}) {
     let selectionRange = new Range(start, end);
 
     if (selectionRange.isEmpty) {
         const end = new Position(textEditor.document.lineCount + 1, 0);
         selectionRange = new Range(new Position(0, 0), end);
     }
+    return selectionRange
+}
 
+function rewriteCodeWithStartAndEnd({
+    start, 
+    end, 
+    textEditor,
+    edit
+}:{
+    start:Position, 
+    end:Position, 
+    textEditor: TextEditor,
+    edit: TextEditorEdit,
+}) {
+    let i = 0;
+    let selectionRange = getSelectionRangeByStartAndEnd({
+        start, 
+        end, 
+        textEditor
+    })
     const beforeText = textEditor.document.getText(selectionRange);
 
     edit.delete(selectionRange);
     let { line, character } = textEditor.selection.start;
+    const initLine = line;
+    const initCharacter = character;
     const recycleWrite = function(inputTimeout: NodeJS.Timeout) {
         isWritingCodeMap.set(textEditor.document.fileName,false)
         clearTimeout(inputTimeout);
     }
+
     const runWrite = function() {
         const inputTimeout: NodeJS.Timeout = setTimeout(() => {
             if(isWriteCodePauseMap.get(textEditor.document.fileName)) {
@@ -62,8 +103,21 @@ function rewriteCode(
                     throw new Error(reason);
                 })
             }
-            if (i >= beforeText.length || textEditor.document.isClosed) {
+            if (textEditor.document.isClosed) {
                 return recycleWrite(inputTimeout)
+            }
+            if(i >= beforeText.length) {
+                if(getRewriteMode()===RewriteMode.Cycle) {
+                    return textEditor.edit((editBuilder) => {
+                        i = 0
+                        editBuilder.delete(selectionRange);
+                        line = initLine;
+                        character = initCharacter;
+                        return runWrite();
+                    })
+                } else {
+                    return recycleWrite(inputTimeout)
+                }
             }
             const nowPosition = new Position(line, character);
             textEditor.edit((editBuilder) => {
@@ -94,7 +148,26 @@ function rewriteCode(
         }, getReWriteSpeed());
     }
     runWrite();
-    
+}
+
+
+function rewriteCode(
+    textEditor: TextEditor,
+    edit: TextEditorEdit,
+    _args: any[]
+) {
+    if(isWritingCodeMap.get(textEditor.document.fileName)) {
+        return window.showInformationMessage('rewriteCode already in progress')
+    }
+    isWritingCodeMap.set(textEditor.document.fileName,true)
+    isWriteCodePauseMap.set(textEditor.document.fileName,false)
+    const { start, end } = textEditor.selection;
+    rewriteCodeWithStartAndEnd({
+        start, 
+        end, 
+        textEditor,
+        edit
+    });
 }
 
 function closeWriteCode(
@@ -119,6 +192,21 @@ function pauseWriteCode(
     showPauseinfo(textEditor);
 }
 
+async function switchWriteMode(
+    _textEditor: TextEditor,
+    _edit: TextEditorEdit,
+    ..._args: any[]
+) {
+    let nowRewriteMode = getRewriteMode();
+    if(nowRewriteMode===RewriteMode.Once) {
+        nowRewriteMode = RewriteMode.Cycle;
+    } else {
+        nowRewriteMode = RewriteMode.Once;
+    }
+    await setRewriteMode(nowRewriteMode)
+    window.showInformationMessage('switch to :' + getRewriteMode())
+}
+
 export function activate(context: ExtensionContext) {
     const textEditorCommandMap = [
         {
@@ -132,6 +220,10 @@ export function activate(context: ExtensionContext) {
         {
             command: 'extension.swimming.pauseWriteCode',
             callback: pauseWriteCode,
+        },
+        {
+            command: 'extension.swimming.switchWriteMode',
+            callback: switchWriteMode,
         },
     ];
 
